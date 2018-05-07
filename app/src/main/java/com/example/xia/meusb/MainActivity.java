709e418ff7042facdc634c5b9b6043662661acff
1392794981 <1392794981@qq.com>
2018-05-07 15:12:30 +0800

    在
    在

--- app/src/main/java/com/example/xia/meusb/MainActivity.java
+++ app/src/main/java/com/example/xia/meusb/MainActivity.java
@@ -13,7 +13,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.media.SoundPool;
@@ -40,6 +42,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -49,6 +52,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.ref.WeakReference;
 import java.math.BigDecimal;
+import java.nio.ByteOrder;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -73,6 +77,26 @@ public class MainActivity extends FragmentActivity {
     String strRecordPath;
     boolean isRecording = false;
 
+    ///--------------------------------
+    private int bufferSizeInShort = 0;//缓冲区大小
+    //音频获取来源
+    private int audioSource = MediaRecorder.AudioSource.MIC;
+    //设置音频的采样率，44100是目前的标准，但是某些设备仍然支持22050,16000,11025
+    private static int sampleRateInHz = 44100;
+    //设置音频的录制声道，CHANNEL_IN_STEREO为双声道，CHANNEL_CONFIGURATION_MONO为单声道
+    private static int channelConfig = AudioFormat.CHANNEL_IN_STEREO;
+    //设置音频数据格式:PCM 16位每个样本，保证设备支持。PCM 8位每个样本，不一定能得到设备的支持。
+    private static int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    //    private static int audioFormat=AudioFormat.ENCODING_PCM_8BIT;
+    //AudioName裸音频数据文件
+    private String AudioName;
+    //NewAudioName可播放的音频文件
+    private String NewAudioName;
+
+    private AudioRecord audioRecord;
+    //播放音频
+    private MediaPlayer mediaPlayer;
+    //====================================
     String strFilePath;
     boolean isShowText = false;
     float playSpeed = 1.0f;
@@ -93,6 +117,285 @@ public class MainActivity extends FragmentActivity {
 
     SortedList pointList;
 
+    //========================================
+    private void creatAudioRecord() {
+        //根据AudioRecord的音频采样率、音频录制声道、音频数据格式获取缓冲区大小
+        bufferSizeInShort = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
+
+        //根据音频获取来源、音频采用率、音频录制声道、音频数据格式和缓冲区大小来创建AudioRecord对象
+        audioRecord = new AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSizeInShort);
+
+        //创建播放实例
+
+    }
+
+    /**
+     * 播放录制的音频
+     */
+    private void playMusic() {
+        File file = new File(NewAudioName);
+        if (file.exists()) {
+            try {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            } catch (Exception e) {
+                txtTemp.setText(e.getMessage());
+            }
+            try {
+//                mediaPlayer.reset();
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setDataSource(NewAudioName);
+                mediaPlayer.prepare();//进行数据缓冲
+                mediaPlayer.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 开始录制音频
+     */
+    private void startAudioRecord() {
+        audioRecord.startRecording();//开始录制
+
+        isRecording = true;
+        btnRecord.setText("停止录音");
+        btnRecord.setBackgroundColor(Color.rgb(0x44, 0x44, 0x44));
+
+        new AudioRecordThread().start();//开启线程来把录制的音频数据保留下来
+    }
+
+    /**
+     * 停止录制音频
+     */
+    private void stopAudioRecord() {
+        close();
+    }
+
+    private void close() {
+        if (audioRecord != null) {
+            System.out.println("stopRecord");
+
+            isRecording = false;
+            txtTemp.setText(strRecordPath);
+            btnRecord.setText("开始录音");
+            btnRecord.setBackgroundResource(R.drawable.buttonstyle);
+
+            audioRecord.stop();
+            audioRecord.release();//释放资源
+            audioRecord = null;
+
+            creatAudioRecord();
+        }
+    }
+
+    /**
+     * 音频数据写入线程
+     *
+     * @author Administrator
+     */
+    class AudioRecordThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            writeDataToFile();//把录制的音频裸数据写入到文件中去
+            copyWaveFile(AudioName, NewAudioName);//给裸数据加上头文件
+        }
+    }
+
+    /**
+     * 把录制的音频裸数据写入到文件中去
+     * 这里将数据写入文件，但是并不能播放，因为AudioRecord获得的音频是原始的裸音频，
+     * 如果需要播放就必须加入一些格式或者编码的头信息。但是这样的好处就是你可以对音频的 裸数据进行处理，比如你要做一个爱说话的TOM
+     * 猫在这里就进行音频的处理，然后重新封装 所以说这样得到的音频比较容易做一些音频的处理。
+     */
+    private void writeDataToFile() {
+        // new一个byte数组用来存一些字节数据，大小为缓冲区大小
+        short[] audioData = new short[bufferSizeInShort];
+        int readSize = 0;
+        FileOutputStream fos = null;
+        File file = new File(AudioName);
+        if (file.exists())
+            file.delete();
+        try {
+            fos = new FileOutputStream(file);//获取一个文件的输出流
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        while (isRecording == true) {
+            readSize = audioRecord.read(audioData, 0, bufferSizeInShort);
+            if (AudioRecord.ERROR_INVALID_OPERATION != readSize) {
+                try {
+                    short temp;
+                    for (int i = 0; i < audioData.length; i++) {
+                        temp = audioData[i];
+                        audioData[i] = (short) (temp<<2);
+                    }
+
+                    fos.write(Shorts2Bytes(audioData));
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            fos.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] Shorts2Bytes(short[] s) {
+        byte bLength = 2;
+        byte[] buf = new byte[s.length * bLength];
+
+        for (int iLoop = 0; iLoop < s.length; iLoop++) {
+            byte[] temp = getBytes(s[iLoop]);
+
+            for (int jLoop = 0; jLoop < bLength; jLoop++) {
+                buf[iLoop * bLength + jLoop] = temp[jLoop];
+            }
+        }
+
+        return buf;
+    }
+
+    public byte[] getBytes(short s) {
+        return getBytes(s, this.testCPU());
+    }
+
+    public byte[] getBytes(short s, boolean bBigEnding) {
+        byte[] buf = new byte[2];
+
+        if (bBigEnding) {
+            for (int i = buf.length - 1; i >= 0; i--) {
+                buf[i] = (byte) (s & 0x00ff);
+                s >>= 8;
+            }
+        } else {
+            for (int i = 0; i < buf.length; i++) {
+                buf[i] = (byte) (s & 0x00ff);
+                s >>= 8;
+            }
+        }
+
+        return buf;
+    }
+
+    public boolean testCPU() {
+        if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN) {
+            // System.out.println("is big ending");
+            return true;
+        } else {
+            // System.out.println("is little ending");
+            return false;
+        }
+    }
+
+    void calc1(short[] lin, int off, int len) {
+        int i, j;
+        for (i = 0; i < len; i++) {
+            j = lin[i + off];
+            lin[i + off] = (short) (j >> 2);
+        }
+    }
+
+    private void copyWaveFile(String inFileName, String outFileName) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = sampleRateInHz;
+        int channels = 2;
+        long byteRate = 16 * sampleRateInHz * channels / 8;
+
+        byte[] data = new byte[bufferSizeInShort*2];
+
+        try {
+            in = new FileInputStream(inFileName);
+            out = new FileOutputStream(outFileName);
+
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen, longSampleRate, channels, byteRate);
+            while (in.read(data) != -1) {
+                out.write(data);
+            }
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 这里提供一个头信息。插入这些信息就可以得到可以播放的文件。
+     * 为我为啥插入这44个字节，这个还真没深入研究，不过你随便打开一个wav
+     * 音频的文件，可以发现前面的头文件可以说基本一样哦。每种格式的文件都有
+     * 自己特有的头文件。
+     */
+    private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                     long totalDataLen, long longSampleRate, int channels, long byteRate)
+            throws IOException {
+        byte[] header = new byte[44];
+        header[0] = 'R'; // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f'; // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1; // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8); // block align
+        header[33] = 0;
+        header[34] = 16; // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+        out.write(header, 0, 44);
+    }
+    //==============================================================
 
     static class HandlerProgress extends Handler {
         WeakReference<MainActivity> mActivity;
@@ -237,6 +540,11 @@ public class MainActivity extends FragmentActivity {
         mediaRecorder = new MediaRecorder();
         recordDialog = new ProgressDialog(MainActivity.this);
         //=====================================
+        AudioName = getApplicationContext().getFilesDir() + "/record..raw";
+        //NewAudioName可播放的音频文件
+        NewAudioName = getApplicationContext().getFilesDir() + "/record..wav";
+        creatAudioRecord();
+        //========================================
         viewPager = findViewById(R.id.viewpager);
         LayoutInflater inflater = getLayoutInflater();
         viewComplex = inflater.inflate(R.layout.layout_complex, null);
@@ -320,7 +628,7 @@ public class MainActivity extends FragmentActivity {
         btnSpeedUp = viewComplex.findViewById(R.id.btnSpeedUp);
         btnRecord = viewComplex.findViewById(R.id.btnRecord);
         btnRecordPlay = viewComplex.findViewById(R.id.btnRecordPlay);
-        btnRecordPlayStop=viewComplex.findViewById(R.id.btnRecordPlayStop);
+        btnRecordPlayStop = viewComplex.findViewById(R.id.btnRecordPlayStop);
 
         txtShowTextInSecond = viewSimple.findViewById(R.id.txtShowTextInSecond);
         btnNextInSecond = viewSimple.findViewById(R.id.btnNextInSecond);
@@ -514,8 +822,10 @@ public class MainActivity extends FragmentActivity {
 
     private void toRecordPlayStop() {
         try {
-            recordPlayer.stop();
-            recordPlayer.release();
+            mediaPlayer.stop();
+            mediaPlayer.release();
+//            recordPlayer.stop();
+//            recordPlayer.release();
         } catch (Exception e) {
             txtTemp.setText(e.getMessage());
             e.printStackTrace();
@@ -716,62 +1026,63 @@ public class MainActivity extends FragmentActivity {
     }
 
     private void toPlayRecord() {
-        try {
-            recordPlayer.stop();
-            recordPlayer.release();
-        } catch (Exception e) {
-            txtTemp.setText(e.getMessage());
-            e.printStackTrace();
-        }
-        try{
-            recordPlayer = new MediaPlayer();
-            recordPlayer.setDataSource(strRecordPath);
-            recordPlayer.prepare();
-            recordPlayer.start();
-            recordPlayer.setVolume(1f, 1f);
-        } catch (Exception e) {
-            txtTemp.setText(e.getMessage());
-            e.printStackTrace();
-        }
+        playMusic();
+
+//        try {
+//            recordPlayer.stop();
+//            recordPlayer.release();
+//        } catch (Exception e) {
+//            txtTemp.setText(e.getMessage());
+//            e.printStackTrace();
+//        }
+//        try {
+//            recordPlayer = new MediaPlayer();
+//            recordPlayer.setDataSource(strRecordPath);
+//            recordPlayer.prepare();
+//            recordPlayer.start();
+//            recordPlayer.setVolume(1f, 1f);
+//        } catch (Exception e) {
+//            txtTemp.setText(e.getMessage());
+//            e.printStackTrace();
+//        }
     }
 
     private void toStopRecord() {
-        try {
-            mediaRecorder.stop();
-            mediaRecorder.reset();
-            isRecording = false;
-            txtTemp.setText(strRecordPath);
-            btnRecord.setText("开始录音");
-            btnRecord.setBackgroundColor(Color.rgb(0x11, 0x11, 0x11));
-            btnRecord.setBackgroundResource(R.drawable.buttonstyle);
-//            recordDialog.dismiss();
-        } catch (Exception e) {
-            txtTemp.setText(e.getMessage());
-        }
+        stopAudioRecord();
+//        try {
+//            mediaRecorder.stop();
+//            mediaRecorder.reset();
+//            isRecording = false;
+//            txtTemp.setText(strRecordPath);
+//            btnRecord.setText("开始录音");
+//            btnRecord.setBackgroundColor(Color.rgb(0x11, 0x11, 0x11));
+//            btnRecord.setBackgroundResource(R.drawable.buttonstyle);
+//        } catch (Exception e) {
+//            txtTemp.setText(e.getMessage());
+//        }
     }
 
     private void toStartRecord() {
-        if (checkPermission()) {
-            try {
-                mediaRecorder.setOutputFile(strRecordPath);
-                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-                mediaRecorder.prepare();
-                mediaRecorder.start();
-                isRecording = true;
-                btnRecord.setText("停止录音");
-                btnRecord.setBackgroundColor(Color.rgb(0x44, 0x44, 0x44));
-                txtTemp.setText("正在录音....");
-//                recordDialog.setMessage("正在录音...");
-//                recordDialog.show();
-            } catch (Exception e) {
-                txtTemp.setText(e.getMessage());
-                e.printStackTrace();
-            }
-        } else {
-            requestPermission();
-        }
+        startAudioRecord();
+//        if (checkPermission()) {
+//            try {
+//                mediaRecorder.setOutputFile(strRecordPath);
+//                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+//                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//                mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+//                mediaRecorder.prepare();
+//                mediaRecorder.start();
+//                isRecording = true;
+//                btnRecord.setText("停止录音");
+//                btnRecord.setBackgroundColor(Color.rgb(0x44, 0x44, 0x44));
+//                txtTemp.setText("正在录音....");
+//            } catch (Exception e) {
+//                txtTemp.setText(e.getMessage());
+//                e.printStackTrace();
+//            }
+//        } else {
+//            requestPermission();
+//        }
     }
 
     private void toSpeedNormal() {
